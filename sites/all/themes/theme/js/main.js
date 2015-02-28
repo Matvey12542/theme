44c97ddf4f1ab862f5764b31d67da9f994f632bc
matvey <mykola.mateychuk@internetdevels.com>
2015-02-28 15:37:47 +0200

    Added files, style slider

--- sites/all/themes/theme/js/main.js
+++ sites/all/themes/theme/js/main.js
@@ -3,13 +3,15 @@
     Drupal.behaviors.bxslider = {
         attach: function (context, settings) {
     $('.front-slider .view-content').addClass('bxslider');
-    front_slider = $('.bxslider').bxSlider({
+    $('.bxslider').bxSlider({
         slideWidth: 1170,
         maxSlides: 1,
         moveSlides: 1,
         slideMargin: 0,
         infiniteLoop: true,
-        touchEnabled:true
+        touchEnabled:true,
+        nextText: '',
+        prevText: ''
     });
 
     console.log(front_slider);
