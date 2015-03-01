124c4fdc897953b4200ce9a230caa99205a7d9d9
matvey <mykola.mateychuk@internetdevels.com>
2015-03-01 13:43:02 +0200

    Added features

--- sites/all/themes/theme/js/main.js
+++ sites/all/themes/theme/js/main.js
@@ -1,20 +1,21 @@
 (function ($) {
-
     Drupal.behaviors.bxslider = {
         attach: function (context, settings) {
-    $('.front-slider .view-content').addClass('bxslider');
-    $('.bxslider').bxSlider({
-        slideWidth: 1170,
-        maxSlides: 1,
-        moveSlides: 1,
-        slideMargin: 0,
-        infiniteLoop: true,
-        touchEnabled:true,
-        nextText: '',
-        prevText: ''
-    });
+            $('.front-slider .view-content').addClass('bxslider');
+            $('.bxslider').bxSlider({
+                slideWidth: 1170,
+                maxSlides: 1,
+                moveSlides: 1,
+                mode: 'fade',
+                slideMargin: 0,
+                infiniteLoop: true,
+                touchEnabled: true,
+                nextText: '',
+                prevText: '',
+                easing: 'ease-out',
+                useCSS: true
+            });
 
-    console.log(front_slider);
-
-        }}
+        }
+    }
 })(jQuery);
