7a96e31afa92306d862cf010a1dc31cf0a4d5488
Maximus <maximus@ubuntu.(none)>
2015-03-03 23:01:56 +0200

    merge conflict

--- sites/all/themes/theme/templates/views-view-unformatted--mixitup-controls--panel-pane-1.tpl.php
+++ sites/all/themes/theme/templates/views-view-unformatted--mixitup-controls--panel-pane-1.tpl.php
@@ -11,8 +11,8 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <?php $data = !empty($mixitup[$id]) ? $mixitup[$id] : ''; ?>
-  <div<?php if ($classes_array[$id]) { print ' class="filter ' . $classes_array[$id] .'" data-filter=".'.$data.'"';  } ?>>
+  <?php $filter = !empty($mixitup[$id]) ? $mixitup[$id] : '';?>
+  <div<?php if ($classes_array[$id]) { print ' class="filter ' . $classes_array[$id] .'"' . $filter;  } ?>>
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>
